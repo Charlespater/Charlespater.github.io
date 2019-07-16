78ea645ef59a0c00f403aa4c5dd59abee81bd780
Charlespater <50410763+Charlespater@users.noreply.github.com>
2019-07-16 17:27:43 +0800

    Update charlespater-lodash.js

--- miao/lodash/charlespater-lodash.js
+++ miao/lodash/charlespater-lodash.js
@@ -2,4 +2,10 @@ var charlespater = {
     compact: function(ary) {
       return ary.filter(it => it)
     },
+   fill: function(ary,value){
+      for(let i=0;i<ary.length;i++){
+        ary.push(value)
+      }
+      return ary;
+    }
   };
